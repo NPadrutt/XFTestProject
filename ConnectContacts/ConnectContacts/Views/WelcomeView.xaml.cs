a56bdec073afc81d32462757d201c003f8c33842
Mikolaj Kieres <Mikolaj Kieres>
2018-04-16 23:05:53 +1000

    Fix for MvvmCross

--- ConnectContacts/ConnectContacts/Views/WelcomeView.xaml.cs
+++ ConnectContacts/ConnectContacts/Views/WelcomeView.xaml.cs
@@ -1,13 +1,15 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using MvvmCross.Forms.Views.Attributes;
+using Xamarin.Forms.Xaml;
 
 namespace ConnectContacts.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class WelcomeView
-	{
-		public WelcomeView ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [MvxContentPagePresentation()]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomeView
+    {
+        public WelcomeView()
+        {
+            InitializeComponent();
+        }
+    }
 }
